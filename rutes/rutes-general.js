46f87959df065c603af428db867cf87295603e6f
Milton Ocampo <miltonf55@hotmail.com>
2020-06-21 23:44:36 -0500

    Estructura completa

--- rutes/rutes-general.js
+++ rutes/rutes-general.js
@@ -8,8 +8,29 @@ app.get("/activaJS", (req, res) => {
 
 app.get("/", (req, res) => {
     res.render("index", {
-        TituloPagina: "Bienvenido"
+        TituloPagina: "Bienvenido",
+        Inicio: "Si"
     });
 });
-
+app.get("/contact", (req, res) => {
+    res.render("contact", {
+        TituloPagina: "Contactanos",
+        Inner: "Si",
+        Contact: "Si"
+    });
+});
+app.get("/service", (req, res) => {
+    res.render("service", {
+        TituloPagina: "Servicios",
+        Inner: "Si",
+        Service: "Si"
+    });
+});
+app.get("/about", (req, res) => {
+    res.render("about", {
+        TituloPagina: "Sobre Nosotros",
+        Inner: "Si",
+        About: "Si"
+    });
+});
 module.exports = app;
